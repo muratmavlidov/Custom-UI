2225ad840175e8f3adfb9b309422c7673b55189e
test <test@MBP-test.Dlink>
2021-05-05 13:51:42 +0300

    initial project

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  ignorePatterns: ["dist", "docs", "tests", "*.config.js"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "react/prop-types": "off",
+    "prefer-spread": "off",
+    "no-console": "error",
+    "react/display-name": "off",
+  },
+};
---
+++ rollup.config.js
@@ -0,0 +1,85 @@
+import path from "path";
+import typescript from "rollup-plugin-typescript2";
+import replace from "@rollup/plugin-replace";
+import { terser } from "rollup-plugin-terser";
+import { sizeSnapshot } from "rollup-plugin-size-snapshot";
+
+const isTesting = process.env.TESTING === "true";
+
+const umdOptions = {
+  name: "Retransition",
+  globals: {
+    react: "React",
+  },
+};
+
+function createRollupConfig(outputDir, tsOptions, env, formats = {}) {
+  const { umd, cjs, es } = formats;
+  return {
+    input: "src/index.ts",
+    plugins: [
+      typescript(tsOptions),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify(env),
+        "process.env.TESTING": JSON.stringify(isTesting),
+      }),
+      !isTesting && sizeSnapshot(),
+      !isTesting && terser(),
+    ].filter(Boolean),
+    external: ["react"],
+    output: [
+      cjs && {
+        file: path.resolve(outputDir, `index.${env}.js`),
+        format: "cjs",
+      },
+      es && {
+        file: path.resolve(outputDir, "index.es.js"),
+        format: "es",
+      },
+      umd && {
+        file: path.resolve(outputDir, "index.umd.js"),
+        format: "umd",
+        ...umdOptions,
+        sourcemap: isTesting ? "inline" : undefined,
+      },
+    ].filter(Boolean),
+  };
+}
+
+const distDir = path.resolve(__dirname, "dist");
+const ie11Dir = path.resolve(__dirname, "dist", "ie11");
+const testDir = path.resolve(__dirname, "tests");
+
+export default isTesting
+  ? createRollupConfig(
+      testDir,
+      {
+        tsconfigOverride: {
+          compilerOptions: {
+            // throws error about declarationDir
+            declaration: false,
+            sourceMap: true,
+            inlineSourceMap: true,
+            inlineSources: true,
+            removeComments: false,
+          },
+        },
+      },
+      "production",
+      { umd: true }
+    )
+  : [
+      createRollupConfig(distDir, undefined, "production", {
+        es: true,
+        umd: true,
+        cjs: true,
+      }),
+      createRollupConfig(distDir, undefined, "development", { cjs: true }),
+      createRollupConfig(
+        ie11Dir,
+        { tsconfig: "tsconfig.ie11.json" },
+        "production",
+        { es: true, umd: true, cjs: true }
+      ),
+      createRollupConfig(ie11Dir, undefined, "development", { cjs: true }),
+    ];
